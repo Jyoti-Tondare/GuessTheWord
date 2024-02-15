94b344f33d79fd75911e4342730b59d2bd3e4325
Jyoti Tondare <tondarejyoti85@gmail.com>
2024-02-15 14:02:55 +0530

    Commited

---
+++ GameApp/src/Application.java
@@ -0,0 +1,13 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		System.out.println("Running........");
+		GuessTheWord game=new GuessTheWord();
+		game.start();
+		game.end();
+
+	}
+
+}
